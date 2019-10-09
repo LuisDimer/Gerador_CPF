ea44d0b752739a301225a1af4b57720043960c78
Luis Fernando Dimer da Silva <luis.silva@herval.com.br>
2019-10-09 08:35:37 -0300

    ajustes

--- scripts/main.js
+++ scripts/main.js
@@ -11,8 +11,6 @@ var nonoDigito = Math.floor(Math.random() * 10);
 
 //Primeiro Dígito Verificador
 var primeiroVerificador = ((primeiroDigito * 10) + (segundoDigito * 9) + (terceiroDigito * 8) + (quartoDigito * 7) + (quintoDigito * 6) + (sextoDigito * 5) + (setimoDigito * 4) + (oitavoDigito * 3) + (nonoDigito * 2)) % 11;
-//Segundo Dígito Verificador
-var segundoVerificador = ((primeiroDigito * 11) + (segundoDigito * 10) + (terceiroDigito * 9) + (quartoDigito * 8) + (quintoDigito * 7) + (sextoDigito * 6) + (setimoDigito * 5) + (oitavoDigito * 4) + (nonoDigito * 3) + (primeiroVerificador * 2)) % 11;
 
 if(primeiroVerificador < 2){
   primeiroVerificador = 0;
@@ -22,6 +20,9 @@ else{
   primeiroVerificador = 11 - primeiroVerificador;
 
 }
+
+//Segundo Dígito Verificador
+var segundoVerificador = ((primeiroDigito * 11) + (segundoDigito * 10) + (terceiroDigito * 9) + (quartoDigito * 8) + (quintoDigito * 7) + (sextoDigito * 6) + (setimoDigito * 5) + (oitavoDigito * 4) + (nonoDigito * 3) + (primeiroVerificador * 2)) % 11;
 
 if(segundoVerificador < 2){
   segundoVerificador = 0;
